4766c20a6d8d8eb965803aa0abc5f4e174144720
ViacheslavMiakota <parazyt2010@gmail.com>
2022-11-27 21:48:05 +0200

    corect

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import { FriendListBox } from './FriendList.styled';
 import FriendListItem from 'components/FriendListItem/FriendListItem';
 const FriendList = ({ friends }) => {
